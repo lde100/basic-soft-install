81d7c0da5ac24b8b5a41a59b2d4b91ae787f5c35
lde100 <89341045+lde100@users.noreply.github.com>
2021-12-13 00:22:39 +0100

    Update package1.ps1

--- package1.ps1
+++ package1.ps1
@@ -1,5 +1,5 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')); 
 SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin";
 choco feature enable -n=allowGlobalConfirmation;
-#choco install googlechrome adobereader microsoft-teams.install spotify vlc irfanview sharex 7zip teamviewer vscode putty.install winscp -y;
+#choco install googlechrome adobereader microsoft-teams.install spotify vlc irfanview sharex 7zip teamviewer vscode putty.install winscp;
 choco install vlc;
