806136a57b0d2f303d68f731ba676b8b37c48628
lde100 <89341045+lde100@users.noreply.github.com>
2021-12-13 13:02:20 +0100

    Update setup1.ps1

--- setup1.ps1
+++ setup1.ps1
@@ -6,9 +6,9 @@ SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin";
 choco feature enable -n=allowGlobalConfirmation;
 #choco install googlechrome adobereader microsoft-teams.install spotify vlc irfanview sharex 7zip teamviewer vscode putty.install winscp;
 
-#Windows Customize
+# Windows Customize
 
-#Disables Web Search in Start Menu
+# Disables Web Search in Start Menu
     Write-Output "Disabling Bing Search in Start Menu"
     $WebSearch = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
     Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" BingSearchEnabled -Value 0 
