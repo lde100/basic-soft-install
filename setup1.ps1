e7a38e7f3f587903ff59bf6b5dcc0b8aa1eb26d6
lde100 <89341045+lde100@users.noreply.github.com>
2021-12-13 14:24:52 +0100

    Update setup1.ps1

--- setup1.ps1
+++ setup1.ps1
@@ -1,6 +1,6 @@
 # Software Install
 
-Write-Output "Install PacketManager and Basic Software"
+Write-Output "`nInstall PacketManager and Basic Software`n"
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')); 
 SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin";
 choco feature enable -n=allowGlobalConfirmation;
@@ -9,7 +9,7 @@ choco feature enable -n=allowGlobalConfirmation;
 # Windows Customize
 
 # Disables Web Search in Start Menu
-    Write-Output "Disabling Bing Search in Start Menu"
+    Write-Output "`nDisabling Bing Search in Start Menu`n"
     $WebSearch = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
     Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" BingSearchEnabled -Value 0 
     If (!(Test-Path $WebSearch)) {
@@ -18,13 +18,13 @@ choco feature enable -n=allowGlobalConfirmation;
     Set-ItemProperty $WebSearch DisableWebSearch -Value 1 
 
 # Enable Dark Mode
-Write-Output "Enable Dark Mode"
+Write-Output "`nEnable Dark Mode`n"
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0 -Type Dword -Force
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -Type Dword -Force
 
 
 # Show Computer Shortcut on Desktop
-Write-Output "Show Computer Shortcut on Desktop"
+Write-Output "`nShow Computer Shortcut on Desktop`n"
 $RegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel"
 $RegKey = "{20D04FE0-3AEA-1069-A2D8-08002B30309D}"
     If (!(Test-Path $RegPath)) {
@@ -33,25 +33,25 @@ $RegKey = "{20D04FE0-3AEA-1069-A2D8-08002B30309D}"
     Set-ItemProperty $RegPath $RegKey -Value 0 -Type Dword -Force
     
 # Disable Taskbar Widgets
-Write-Output "Disable Taskbar Widgets"
+Write-Output "`nDisable Taskbar Widgets`n"
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarDa -Value 0 -Type Dword -Force
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarMn -Value 0 -Type Dword -Force
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name ShowTaskViewButton -Value 0 -Type Dword -Force
 
 # Disable Taskbar Search
-Write-Output "Disable Taskbar Search"
+Write-Output "`nDisable Taskbar Search`n"
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Search -Name SearchboxTaskbarMode -Value 0 -Type Dword -Force
 
 # Set File Explorer to open This PC
-Write-Output "Set File Explorer to open This PC"
+Write-Output "`nSet File Explorer to open This PC`n"
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1 -Type Dword -Force
 
 # Show File Extensions
-Write-Output "Show File Extensions"
+Write-Output "`nShow File Extensions`n"
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -Value 0 -Type Dword -Force
 
 # Rename This PC
-Write-Output "Rename This PC"
+Write-Output "`nRename This PC`n"
 $RegPath = "HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{20D04FE0-3AEA-1069-A2D8-08002B30309D}"
 $RegKey = "(Default)"
 $RegValue = "Arbeitsplatz "+ $env:COMPUTERNAME
@@ -61,7 +61,7 @@ $RegValue = "Arbeitsplatz "+ $env:COMPUTERNAME
     Set-ItemProperty $RegPath $RegKey -Value $RegValue -Type String -Force
     
 # Rename System Drive
-Write-Output "Rename System Drive"
+Write-Output "`nRename System Drive`n"
 $RegPath = "HKCU:Software\Classes\Applications\Explorer.exe\Drives\C\DefaultLabel"
 $RegKey = "(Default)"
 $RegValue = "SYSTEM "+ $env:COMPUTERNAME
