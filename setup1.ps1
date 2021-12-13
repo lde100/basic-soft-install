7e2cd36d4762c9b0df702d68347ede004ac03c0e
lde100 <89341045+lde100@users.noreply.github.com>
2021-12-13 13:59:38 +0100

    Update setup1.ps1

--- setup1.ps1
+++ setup1.ps1
@@ -52,10 +52,10 @@ Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\
 
 # Rename This PC
 Write-Output "Rename This PC"
-$RegPath = "HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID"
-$RegKey = "{20D04FE0-3AEA-1069-A2D8-08002B30309D}"
+$RegPath = "HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{20D04FE0-3AEA-1069-A2D8-08002B30309D}"
+$RegKey = "LocalizedString"
 $RegValue = "Arbeitsplatz "+ $env:COMPUTERNAME
     If (!(Test-Path $RegPath)) {
         New-Item $RegPath
     }
-    Set-ItemProperty $RegPath $RegKey -Value $RegValue -Type String -Force
+    Set-ItemProperty $RegPath $RegKey -Value $RegValue -Type ExpandString -Force
