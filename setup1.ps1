da7b83d6db94a446b425037d8f1cdc92399d10dc
lde100 <89341045+lde100@users.noreply.github.com>
2021-12-13 12:01:23 +0100

    Update setup1.ps1

--- setup1.ps1
+++ setup1.ps1
@@ -25,5 +25,10 @@ Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Pe
 
 # Show Computer Shortcut
 Write-Output "Show Computer Shortcut"
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\NonEnum -Name "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" -Value 0 -Type Dword -Force
-
+#Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\NonEnum -Name "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" -Value 0 -Type Dword -Force
+$RegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\NonEnum"
+$RegKey = "{20D04FE0-3AEA-1069-A2D8-08002B30309D}"
+    If (!(Test-Path $RegPath)) {
+        New-Item $RegPath
+    }
+    Set-ItemProperty $RegPath $RegKey -Value 0 -Type Dword -Force
