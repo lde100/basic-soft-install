3a34c467c665dda96100776c958aea0cfe98db93
lde100 <89341045+lde100@users.noreply.github.com>
2021-12-13 12:19:26 +0100

    Update setup1.ps1

--- setup1.ps1
+++ setup1.ps1
@@ -1,6 +1,6 @@
 # Software Install
 
-Write-Output "Install PacketManager and Basic Software..."
+Write-Output "Install PacketManager and Basic Software"
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')); 
 SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin";
 choco feature enable -n=allowGlobalConfirmation;
@@ -9,7 +9,7 @@ choco feature enable -n=allowGlobalConfirmation;
 #Windows Customize
 
 #Disables Web Search in Start Menu
-    Write-Output "Disabling Bing Search in Start Menu..."
+    Write-Output "Disabling Bing Search in Start Menu"
     $WebSearch = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
     Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" BingSearchEnabled -Value 0 
     If (!(Test-Path $WebSearch)) {
@@ -23,9 +23,8 @@ Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Pe
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -Type Dword -Force
 
 
-# Show Computer Shortcut
-Write-Output "Show Computer Shortcut"
-#Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\NonEnum -Name "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" -Value 0 -Type Dword -Force
+# Show Computer Shortcut on Desktop
+Write-Output "Show Computer Shortcut on Desktop"
 $RegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel"
 $RegKey = "{20D04FE0-3AEA-1069-A2D8-08002B30309D}"
     If (!(Test-Path $RegPath)) {
