25c29402002441186707b75f2d285e9ea64a05f5
lde100 <89341045+lde100@users.noreply.github.com>
2021-12-15 11:07:19 +0100

    Update setup1.ps1

--- setup1.ps1
+++ setup1.ps1
@@ -4,7 +4,8 @@ Write-Output "`nInstall PacketManager and Basic Software:`n"
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')); 
 SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin";
 choco feature enable -n=allowGlobalConfirmation;
-choco install chocolateygui googlechrome adobereader microsoft-teams.install discord.install obs-studio.install drawio spotify vlc irfanview irfanview-languages irfanviewplugins sharex bitwarden 7zip advanced-renamer.install mediainfo teamviewer vscode.install putty.install filezilla winscp.install;
+#MS Teams installation über Choco macht Probleme: Paket  microsoft-teams.install entfernt
+choco install chocolateygui googlechrome adobereader discord.install obs-studio.install drawio spotify vlc irfanview irfanview-languages irfanviewplugins sharex bitwarden 7zip advanced-renamer.install mediainfo teamviewer vscode.install putty.install filezilla winscp.install;
 
 # Windows Customize
 
