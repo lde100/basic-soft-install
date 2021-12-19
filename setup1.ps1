f4b0fbaf98d9f33c8af1789a8dbd70124cc868d7
lde100 <89341045+lde100@users.noreply.github.com>
2021-12-19 18:17:15 +0100

    Update setup1.ps1

--- setup1.ps1
+++ setup1.ps1
@@ -5,7 +5,11 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/in
 SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin";
 choco feature enable -n=allowGlobalConfirmation;
 #MS Teams installation über Choco macht Probleme: Paket  microsoft-teams.install entfernt
-choco install chocolateygui googlechrome adobereader discord.install obs-studio.install drawio spotify vlc irfanview irfanview-languages irfanviewplugins sharex bitwarden 7zip advanced-renamer.install mediainfo teamviewer vscode.install putty.install filezilla winscp.install;
+#Full Setup:
+#choco install chocolateygui googlechrome adobereader discord.install obs-studio.install drawio spotify vlc irfanview irfanview-languages irfanviewplugins sharex bitwarden 7zip advanced-renamer.install mediainfo teamviewer vscode.install putty.install filezilla winscp.install;
+
+#Basic Setup: 
+choco install chocolateygui googlechrome adobereader vlc irfanview irfanview-languages irfanviewplugins 7zip vscode.install;
 
 # Windows Customize
 
