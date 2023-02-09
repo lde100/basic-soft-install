782a19531c3ddec807583ba2111c32fb9eead98c
lde100 <89341045+lde100@users.noreply.github.com>
2023-02-09 11:13:09 +0100

    Update setup1.ps1

--- setup1.ps1
+++ setup1.ps1
@@ -38,6 +38,10 @@ Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\
 Write-Output "Disable Taskbar Search`n"
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Search -Name SearchboxTaskbarMode -Value 0 -Type Dword -Force
 
+# Disable Lockscreen Tips
+Write-Output "Disable Lockscreen Tips`n"
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name SubscribedContent-338387Enabled -Value 0 -Type Dword -Force
+
 # Set File Explorer to open This PC
 Write-Output "Set File Explorer to open This PC`n"
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1 -Type Dword -Force
@@ -46,9 +50,13 @@ Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\
 Write-Output "Show File Extensions`n"
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -Value 0 -Type Dword -Force
 
+# Restore Win 10 Context Menue
+Write-Output "Restore Win 10 Context Menue`n"
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
 # Show All Folder in Navigation Panel
-Write-Output "Show All Folder in Navigation Panel`n"
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1 -Type Dword -Force
+#Write-Output "Show All Folder in Navigation Panel`n"
+#Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1 -Type Dword -Force
 
 # Rename This PC Shortcut
 Write-Output "Rename This PC Shortcut`n"
@@ -73,4 +81,81 @@ $RegValue = "SYSTEM "+ $env:COMPUTERNAME
         New-Item $RegPath
     }
     Set-ItemProperty $RegPath $RegKey -Value $RegValue -Type String -Force
+    
+    
+    Write-Host "Uninstalling default apps"
+$apps = @(
+            "CandyCrushSaga" 
+            "CandyCrush" # CandyCrushSaga
+            "wallet" 
+            "Microsoft Pay"  # Microsoft Wallet (Microsoft Pay)
+            "skypeapp"       # App Get Skype
+            "officehub"      # App Get Office
+            "getstarted"     # Get Started or Tips
+            "solitaire"      # Microsoft Solitaire Collection
+             #"bing"           # Money, News, Sports and Weather apps together
+            "bingfinance"    # Money
+             #"bingweather"    # Weather
+            "bingnews"       # News
+            "bingsports"     # Sports
+            "onenote"        # OneNote
+            "sway"           # Sway
+            "oneconnect"     # Paid Wi-Fi & Cellular
+            "cortana"        # Cortana used to be Non-Removable but is removable on some version of Windows
+            "Microsoft.549981C3F5F10"
+            "Cortana" # Alias for cortana on some version of Windows
+            "Help"
+            "tiktok"
+            "twitter"
+            "instagram"
+            "BubbleWitchSaga"
+            "MarchOfEmpires"
+            "Disney"
+            "spotify"
+            "hulu"
+            "photoshop"
+            "Clipchamp.Clipchamp"
+            "picsart"
+            "Facebook"
+            "phone"          # Phone and Phone Companion apps together
+            "feedback" 
+            "Feedback Hub" # Feedback Hub
+            "xbox"           # All Xbox Apps
+            "xboxapp"
+            "XBox"
+            "Microsoft.XboxGamingOverlay"
+            "GamingApp"
+            #"communicationsapps"  # Calendar and Mail apps together
+            "todos"
+            "maps"           # Maps
+            "messaging"      # Messaging and Skype Video apps together
+            #"onedrive"       # OneDrive Connect
+            "connectivitystore"  # Microsoft Wi-Fi
+            "mspaint"        # Paint 3D
+            "people"         # People
+            "sticky"         # Sticky Notes
+            "3dbuilder"      # 3D Builder
+            "3d"             # View 3D
+            "soundrecorder"  # Voice Recorder
+            "holographic"    # Windows Holographic
+            "mixedReality"
+            "Mixed Reality Portal" 
+            "Dolby"
+            "Teams"
+            "zune"           # Groove Music and Movies & TV apps together
+            "zunemusic"      # Groove Music
+            "zunevideo"      # Movies & TV
+            
+
+)
+foreach ($app in $apps) {
+    #Simple Version     
+    #get-appxpackage -AllUsers -ErrorAction SilentlyContinue *$app* | remove-appxpackage
+    $package = Get-AppxPackage -Name *$app* -AllUsers -ErrorAction SilentlyContinue
+    if ($package) { 
+        Remove-AppxPackage -Package $package.PackageFullName
+        Get-AppXProvisionedPackage -Online | where DisplayName -EQ $app |`
+            Remove-AppxProvisionedPackage -Online
+    }
+}
 
